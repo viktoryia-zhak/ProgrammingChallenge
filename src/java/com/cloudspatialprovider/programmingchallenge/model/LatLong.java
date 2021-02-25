354d0d2e0afaa9e6631fd62c61f27f56a3314331
Viktoryia.Zhak <viktoryi>
2021-02-25 14:45:30 +0300

    add model class for saving longitude and latitude

---
+++ src/java/com/cloudspatialprovider/programmingchallenge/model/LatLong.java
@@ -0,0 +1,16 @@
+package com.cloudspatialprovider.programmingchallenge.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * LatLong.
+ *
+ * @author Viktoryia Zhak
+ */
+@Data
+@AllArgsConstructor
+public class LatLong {
+    private int longitude;
+    private int latitude;
+}
